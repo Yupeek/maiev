657d3cb09cd9bbf13f019cdecf8f6d201b261339
darius BERNARD <darius@yupeek.com>
2017-06-09 11:59:03 +0200

    add in scaler_docker support for auth to registry.

---
+++ services/scaler/scaler_docker/app/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+
+# add credentials if given
+if [ ! -z "$DOCKER_CREDENTIALS_SECRET" ];
+then
+	echo "add credentials from secret $DOCKER_CREDENTIALS_SECRET"
+	mkdir -p $HOME/.docker || /bin/true
+	ln -s "/run/secrets/$DOCKER_CREDENTIALS_SECRET" "$HOME/.docker/config.json"
+fi
+
+exec nameko run --config config.yaml service.scaler_docker.scaler_docker
