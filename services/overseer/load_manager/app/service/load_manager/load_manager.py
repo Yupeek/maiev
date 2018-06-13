6f0dc53f5b3a16e53217e20f39091abe4337774b
darius BERNARD <darius@yupeek.com>
2018-06-13 15:44:18 +0200

    fix keyerror on staging.

--- services/overseer/load_manager/app/service/load_manager/load_manager.py
+++ services/overseer/load_manager/app/service/load_manager/load_manager.py
@@ -126,7 +126,8 @@ class LoadManager(BaseWorkerService):
             service,
             upsert=True,
         )
-        self._set_trigger_rules(service['name'], service['scale_config']['scale'])
+        if 'scale' in service['scale_config']:
+            self._set_trigger_rules(service['name'], service['scale_config']['scale'])
 
     @rpc
     @log_all
